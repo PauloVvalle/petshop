233638c747bcc6b900640bd6dee44ecf85abf00f
pauloVvalle <paulo1111555@outlook.com>
2024-01-23 19:23:22 -0300

    aa

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -3,7 +3,7 @@ import React from "react"
 import AdminLayout from './layout';
 const Dashboard = () => {
   return <main className="mx-auto max-w-5xl text-2xl gap-2 my-10">
-    aaaaDashboard
+    contrução
 
 
     </main>
